bd423ad7733b9251e7de8cbac11234a1519e0e37
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-08-25 20:09:19 +0000

    indexstats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,24 @@
 """index"""
 from api.v1.views import app_views
 
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def api_stats():
+    """Return statistics about data objects."""
+    statistics = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(statistics)
+
 
 @app_views.route("/status")
 def api_status():
