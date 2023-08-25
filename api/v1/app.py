f2952820b34098e0a925c5b87eac120b6bb6e704
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-08-25 18:19:04 +0000

    4

---
+++ api/v1/app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Main module for the API"""
+
+from api.v1.views import app_views
+from os import getenv
+from flask import Flask, jsonify
+from flask_cors import CORS
+from models import storage
+
+
+# Flask instance
+app = Flask(__name__)
+
+# Register the blueprint for API views
+app.register_blueprint(app_views)
+
+# Allow cross-origin requests
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
+# Close database
+@app.teardown_appcontext
+def close_db(error):
+    """Close the database connection after each request."""
+    storage.close()
+
+# Error 404
+@app.errorhandler(404)
+def handle_404(error):
+    """Handle 404 errors."""
+    return jsonify({"error": "Not found"}), 404
+
+# Run the app if executed directly
+if __name__ == "__main__":
+    app.run(
+        host=getenv("HBNB_API_HOST", "0.0.0.0"),
+        port=int(getenv("HBNB_API_PORT", "5000")),
+        threaded=True,
+        debug=True
+    )
