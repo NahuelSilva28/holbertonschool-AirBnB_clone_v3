890f3a14d49bde2130a38f6c57caae7bf94b1667
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-08-27 19:50:00 +0000

    db

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -75,14 +75,14 @@ class DBStorage:
         """call remove() method on the private session attribute"""
         self.__session.remove()
 
-def get(self, cls, id):
-    """Retrieve an object based on class and ID"""
-    if cls and id:
-        return self.__session.query(cls).get(id)
-    return None
+    def get(self, cls, id):
+        """Retrieve an object based on class and ID"""
+        if cls and id:
+            return self.__session.query(cls).get(id)
+        return None
 
-def count(self, cls=None):
-    """Count the number of objects in storage"""
-    if cls:
-        return self.__session.query(cls).count()
-    return sum(self.count(c) for c in classes.values())
+    def count(self, cls=None):
+        """Count the number of objects in storage"""
+        if cls:
+            return self.__session.query(cls).count()
+        return sum(self.count(c) for c in classes.values())
